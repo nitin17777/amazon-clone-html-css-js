b2811866994d5117bc0a57f2de2be9bf39abe39b
Nitin <puneranitin@gmail.com>
2025-08-22 14:22:15 +0530

    Add jasmine testing framework

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,41 +1,27 @@
-export
-const deliveryOptions = [
-
-    {
-        id: '1',
-        deliveryDays : 7,
-        prcieCents:0
-    },
-    {
-        id: '2',
-        deliveryDays : 3,
-        prcieCents:499
-    },
-    {
-        id: '3',
-        deliveryDays : 1,
-        prcieCents:999
-    },
-
-
-
-    
+export const deliveryOptions = [
+  {
+    id: '1',
+    deliveryDays: 7,
+    priceCents: 0
+  },
+  {
+    id: '2',
+    deliveryDays: 3,
+    priceCents: 499
+  },
+  {
+    id: '3',
+    deliveryDays: 1,
+    priceCents: 999
+  }
 ];
 
+export function getDeliveryOption(deliveryOptionId) {
+  // find matching delivery option by ID
+  const deliveryOption = deliveryOptions.find(
+    option => option.id === deliveryOptionId
+  );
 
-
-export function getDeliveryOption()
-{
-    let deliveryOption;
-
-      deliveryOptions.forEach((option) =>
-      {
-
-        if(option.id === deliveryOptionId )
-        {
-          deliveryOption = option;
-        }
-      });
-
-      return deliveryOption || deliveryOptions[0];
+  // if not found, return default (first one)
+  return deliveryOption || deliveryOptions[0];
 }
