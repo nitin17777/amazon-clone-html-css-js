628e3d85049b4e26d968f6cc65c55e3c2ca70902
Nitin <puneranitin@gmail.com>
2025-08-27 14:52:20 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -83,7 +83,7 @@ class Product
 // }
 
 
-
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -751,4 +751,34 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails); 
+});*/
+
+
+export let products = [];
+
+
+export function loadProducts(fun)
+{
+  const xhr =new XMLHttpRequest();
+
+  xhr.addEventListener('load', () =>
+  {
+   products =  JSON.parse(xhr.response)
+   .map((productDetails)=>{
+
+
+  if(productDetails.type === 'clothing')
+  {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails); 
 });
+
+  });
+  
+  fun();
+  
+  xhr.open('GET', 'https://supersimplebackend.dev');
+  xhr.send();
+}
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,15 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts} from '../data/products.js';
+
 
 //import '../data/cart-class.js';
 
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
+loadProducts(
+    () => {
 
-renderOrderSummary();
-renderPaymentSummary();
-
-
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
